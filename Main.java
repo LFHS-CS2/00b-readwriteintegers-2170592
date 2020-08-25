38cc9c7eff1e3f1c901e55e0a5f504de49e9aef3
Adrian Ye <2170592@lfschools.net>
2020-08-25 18:56:11 +0000

    I did it.

--- Main.java
+++ Main.java
@@ -1,7 +1,14 @@
 import java.io.*;
 import java.util.*;
 class Main {
-    public static void main(String[] args) throws FileNotFoundException, IOException {
-
+  public static void main(String[] args) throws FileNotFoundException, IOException {
+    Scanner in = new Scanner(new File("test.in"));
+    PrintWriter out = new PrintWriter("test.out");
+    while (in.hasNextInt()) {
+      out.println(1+in.nextInt());
     }
+
+    in.close();
+    out.close();
+  }
 }
